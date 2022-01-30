925ddf43fdf1a32bc2338783cfec0b816bbb919b
playyard <90937279+playyard@users.noreply.github.com>
2022-01-30 12:03:18 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: 'User Acquisition News',
-  domain: 'uanews',
+  domain: 'adscore.xyz',
   author: 'Alex',
 
   // open graph metadata (optional)
